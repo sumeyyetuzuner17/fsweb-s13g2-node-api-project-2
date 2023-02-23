232b0a21e568973105202e6bbf76caaf9fcf670f
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-02-23 08:42:02 +0300

    henüz tamamlanmadı

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // posts için gerekli routerları buraya yazın
+const express = require("express");
+const Post = require("./posts-model");
+
+const router = express.Router();
+
+router.get("./", (req, res) => {
+  Post.find()
+    .then((posts) => {
+      res.json(post);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Gönderiler alınamadı" });
+    });
+});
+
+module.exports=router;
